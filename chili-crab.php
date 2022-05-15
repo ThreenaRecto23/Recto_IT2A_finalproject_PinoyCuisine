57eb5481793edd6d9b1e91b805e0098b4df1d020
ThreenaRecto23 <101386811+ThreenaRecto23@users.noreply.github.com>
2022-05-15 13:17:43 +0800

    chili-crab added

---
+++ chili-crab.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>Pinoy Cuisine</title>
+	<link rel="icon" type="image/x-icon" href="images/logo-2.png" />
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400&family=Ubuntu:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">    <!---CDN Link---->
+</head>
+<body>
+
+	<section class="sub-header">
+		<nav>
+			<a href="index.php"><img src="images/Pinoy-Cuisine.png"></a>
+			<div class="nav-links" id="navLinks">
+				<i class="fa fa-times" onclick="hideMenu()"></i>  <!---ICON for closed---->
+				<ul>
+					<li><a href="index.php">HOME</a></li>
+					<li><a href="menu.php" class="active">MENU</a></li>
+					<li><a href="about.php">ABOUTP</a></li>
+				</ul>
+
+			</div>
+			<i class="fa fa-bars" onclick="showMenu()"></i> <!---ICON for navigation menu---->
+		</nav>
+
+		<h1>MENUS</h1>
+		<p>Believe in yourself, you can make good food and great vibes.</p>
+
+</section>
+
+
+<!--------Menu------------>
+
+<section class="menu">
+	<h1>CHILI CRAB</h1>
+	<p>Chili crab is a popular seafood dish in Philippines. This dish is composed of crab cooked in sweet and spicy sauce. </p>
+	
+	
+	<div class="row">
+		<div class="menu-col-1" align="justify">
+			<h3>INGREDIENTS:</h3>
+			<?php  
+
+			echo "<p> * 2 lbs crab, cut in half</p><br>"; 
+			echo "<p> * 2 tbsp cooking oil</p><br>"; 
+			echo "<p> * 2 tbsp garlic, minced</p><br>";
+			echo "<p> * 3 tbsp ginger, minced </p><br>";
+			echo "<p> * 1 tbsp crushed red pepper or 3 pieces sliced red chili</p><br>";
+			echo "<p> * 2 tbsp hoisin sauce </p><br>";
+			echo "<p> * 1/2 cup tomato catsup or tomato sauce</p><br>";
+			echo "<p> * 1/4 cup sweet chili sauce</p><br>";
+			echo "<p> * 2 tbsp fish sauce</p><br>";
+			echo "<p> * 1/2 tsp sesame oil</p><br>";
+			echo "<p> * 1/4 cup water</p><br>";
+			echo "<p> * 3 tbsp gereen onions or scallions, thinly sliced crosswise</p><br>";
+			
+			?>
+		</div>
+
+		<div class="menu-col-1" align="justify">
+			<h3>PROCEDURE:</h3>
+			<?php  
+
+			echo "<p> <b>Step 1:</b> Heat the cooking pot and pour thew cooking oil</p><br>"; 
+			echo "<p> <b>Step 2:</b> Saute the garlic, ginger, and crushed red pepper</p><br>"; 
+			echo "<p> <b>Step 3:</b> Add the crab and cook for 3 to 4 minutes</p><br>";
+			echo "<p> <b>Step 4:</b> Put-in the hoisin sauce, tomato catsup, sweet chili sauce, fish sauce, and sesame oil then mix with the other ingredients</p><br>";
+			echo "<p> <b>Step 5:</b> Pour the water and bring to a boil then simmer for 10 minutes or until the sauce is thick</p><br>";
+			echo "<p> <b>Step 6:</b> Garnsih with green onions on top</p><br>";
+			echo "<p> <b>Step 7:</b> Serve hot. Share and Enjoy! </p><br>";
+			?>
+		</div>
+		
+
+	</div>
+
+</section>
+
+
+<!---------Photos-------->
+<section class="photos">
+	<h1>FOOD IMAGE</h1>
+
+	<div class="pic">
+		<img src="images/chili-crab.png">
+	</div>
+</section>
+
+
+
+
+
+<!--------Footer------------>
+<section class="footer">
+	<h4> ABOUT US</h4>
+	<p>This website is best for those people who loves to cook and learn how to cook. <br> It was created to better represent the Philippines most popular dishes. This website also provides culinary recipes and ingredients.  </p><br>
+	<h4>For Video Tutorials</h4>
+	<div class="icons">
+		<i class="fa fa-facebook" ></i>
+		<i class="fa fa-youtube-play" ></i>
+	</div>
+</section>
+
+
+
+<!------JavaScript for Toggle menu------>
+<script>
+	var navLinks = document.getElementById("navLinks")
+
+	function showMenu(){
+		navLinks.style.right ="0";
+	}
+
+	function hideMenu(){
+		navLinks.style.right ="-200px";
+	}
+
+</script>
+
+
+
+
+</body>
+</html>
